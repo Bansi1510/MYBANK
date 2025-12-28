a9235fe8537de885354a8687f4ac0223fa6c8941
prajapati bansi <prajapatibansi1510@gmail.com>
2025-12-28 20:04:40 +0530

    slove user login problem

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,30 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../shared/Navbar";
 import Hero from "../Home/Hero";
 import Features from "../Home/Features";
 import Services from "../Home/Services";
 import Stats from "../Home/Stats";
 import Footer from "../shared/Footer";
-import Cookies from "js-cookie";
-import { getProfileAPI } from "../services/user.service";
-import { useDispatch } from "react-redux";
-import { clearAuth, setAuth } from "../redux/slices/authSlice";
+
 
 const Home: React.FC = () => {
-   const token = Cookies.get("token");
-   console.log(token);
-   const dispatch = useDispatch();
-   useEffect(() => {
-      const fetchData = async () => {
-         if (token) {
-            const profile = await getProfileAPI();
-            if (profile) dispatch(setAuth(profile));
-         } else {
-            dispatch(clearAuth());
-         }
-      }
-      fetchData();
-   }, []);
+
    return <>
       <Navbar />
       <Hero />
